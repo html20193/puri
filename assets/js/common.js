32af4d894d4584aa33b51b54b38350a4134edf95
html20193 <58695492+html20193@users.noreply.github.com>
2020-01-16 16:57:29 +0900

    brand
    sub/brand.html 수정

--- assets/js/common.js
+++ assets/js/common.js
@@ -50,10 +50,13 @@ $(document).ready(function () {
                     $('.btn_menu').focus();
                 }
             });
+            $dim.on('click', function () {
+                $('.btn_menu').click();
+            });
         }
 
         //depth1 a click
-        $gnb.find('>ul>li>a').on('click', function () {
+        $gnb.find('ul.fold>li>a').on('click', function () {
             if ($(this).next().size() == 0) { //depth1 <a>만 있는 경우
                 //console.log($(this).next().size());
                 location.href = $(this).attr("href");
@@ -65,9 +68,6 @@ $(document).ready(function () {
             return false;
         });
 
-        $dim.on('click', function () {
-            $('btn_menu').click();
-        });
     });
 
     //search bar
@@ -84,7 +84,7 @@ $(document).ready(function () {
         $search.stop().slideUp(200);
     });
 
-
+    // swiper
     var bnrSwiper = new Swiper('.main_visual_bnr .swiper-container', {
         slidesPerView: 'auto',
         freeMode: true,
@@ -95,7 +95,87 @@ $(document).ready(function () {
         },
     });
     var productSwiper = new Swiper('.main_bestsellor .swiper-container', {
-        slidesPerView: 3,
+        slidesPerView: 'auto',
         freeMode: true,
+    });
+
+    var brandSwiper = new Swiper('#aboutPuri .swiper-container', {
+        pagination: {
+            el: '.swiper-pagination.progresspg',
+            type: 'progressbar',
+        },
+        pagination: {
+            el: '.swiper-pagination.bulletpg',
+            clickable: true,
+            renderBullet: function (index, className) {
+              return '<span class="' + className + '">' + (index + 1) + '</span>';
+            },
+        },
+    });
+
+    var productSwiper = new Swiper('#skincare .swiper-container', {
+        slidesPerView: 'auto',
+        freeMode: true,
+    });
+
+    /* tab */
+    //1) 로딩이 완료된후 초기화면 설정
+    $('.tab:first-of-type, .tabpanel:first-of-type').addClass('active').attr('tabIndex', 0);
+    $('.tab:first-of-type').attr('aria-selected', true);
+    $('.tabpanel:first-of-type').attr('aria-hidden', false);
+
+    /* 2) 탭버튼에서 키보드를 누르는 이벤트(keydown) - 키보드 제어*/
+    $('.tab').on('keydown', function (e) {
+        var key = e.keyCode;
+        console.log(key); //왼쪽방향키 37 , 오른쪽 방향키 39, 스페이스바 32 , 엔터 13
+        switch (key) {
+        case 37:    //왼쪽 방향키
+            $(this).attr('tabIndex', -1);
+            if ($(this).hasClass('first')) $(this).siblings('.last').attr('tabIndex', 0).focus();
+            else $(this).prev().attr('tabIndex', 0).focus();
+            break;
+        case 39:  //오른쪽 방향키
+            $(this) .attr('tabIndex', -1);
+            if ($(this).hasClass('last')) $(this).siblings('.first').attr('tabIndex', 0).focus();
+            else $(this).next().attr('tabIndex', 0).focus();
+            break;
+        case 36: //home
+            e.preventDefault(); //기본기능 차단
+            $(this).siblings('.first').attr('tabIndex', 0).focus();
+            break;
+        case 35: //end
+            e.preventDefault(); //기본기능 차단
+            $(this).siblings('.first').attr('tabIndex', 0).focus();
+            break;
+        case 32:  //스페이스바
+        case 13:  //엔터
+            var $tg = $(this);
+            activeOn($tg);
+            break;
+        }
+    });
+
+    //3) 탭 클릭 이벤트
+    $('.tab').on('click', function () {
+        var $tg = $(this);
+        activeOn($tg);
+    });
+
+    function activeOn($target) {
+        $target.addClass('active').attr({'aria-selected': true, tabIndex: 0}).siblings().removeClass('active').attr({'aria-selected': false, tabIndex: -1});
+        $('#' + $target.attr('aria-controls')).addClass('active').attr({'aria-hidden': false, tabIndex: 0}).siblings('.tabpanel').removeClass('active').attr({'aria-hidden': true, tabIndex: -1});
+    }
+
+    //sub pages
+    var $brandTab = $('#brandTab .tabpanel ul li');
+    var tabimgsize = $brandTab.find('img').outerHeight(true);
+    console.log(tabimgsize);
+    $brandTab.find('img').addClass('boxarea');
+    
+    $(window).on('resize', function () {
+        if (tabimgsize > 133) $brandTab.hasClass('even').children('p').css({top: tabimgsize});
+
+        if ($('#skincareItems .price').width() < 150) $('#skincareItems .price .dc').addClass('nonvisible');
+        else $('#skincareItems .price .dc').removeClass('nonvisible');    
     });
 });
